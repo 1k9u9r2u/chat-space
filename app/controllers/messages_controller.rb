3f1cec02079460b7e08fa9a484babd2ce2829494
1k9u9r2u <age1k9u9r2u@yahoo.co.jp>
2019-12-13 15:02:17 +0900

    syuusei

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,13 +4,16 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json { @new_message = @messages.where('id > ?', params[:last_id])}
+    end
   end
+  
   
   def create
     @message = @group.messages.new(message_params)
-    
     if @message.save
-      
       respond_to do |format|
         format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
         format.json
