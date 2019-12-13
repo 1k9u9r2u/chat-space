01bf001a936b5d7a3a5b07daa4eead96fbe2f27a
1k9u9r2u <age1k9u9r2u@yahoo.co.jp>
2019-12-13 15:27:54 +0900

    syuusei

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20191129110741) do
   end
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content"
+    t.string   "image"
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
